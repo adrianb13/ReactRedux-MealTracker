7cd87cd117a9880143df3f42866f9e921c539448
Adrian <coding4adrian@yahoo.com>
2020-02-07 19:36:30 -0800

    home page styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 //import * as actions from "../../actions/index";
+import "./home.css";
 
 class Home extends React.Component {
   constructor(props){
@@ -9,7 +10,8 @@ class Home extends React.Component {
 
     this.state = {
       trackers: [],
-      meals: []
+      meals: [],
+      foods: []
     }
   };
 
@@ -25,73 +27,93 @@ class Home extends React.Component {
 	};
 
   dataLoad = () => {
-    if(this.props.trackers){
+    if(this.props.trackers.length !== 0){
       this.setState({
         trackers: this.props.trackers
       }, () => {
-        console.log(this.state.trackers);
-      })
-    }
-    console.log(this.props.trackers[0])
-    if(this.props.trackers.meals){
-      this.setState({
-        meals: this.props.trackers.meals
-      }, () => {
-        console.log(this.state.meals);
+        this.listMeals(1);
       })
     }
   };
 
+  listMeals = (trackerId) => {
+    let tracker = this.state.trackers.filter(tracker => tracker.id === trackerId);
+    if(tracker[0].meals.length !== 0){
+      this.setState({
+        meals: tracker[0].meals
+      }, () => {
+        console.log(this.state.meals);
+        this.listFood(1);
+      })
+    }
+  }
+
+  listFood = (mealId) => {
+    let meal = this.state.meals.filter(meal => meal.id === mealId);
+    if(meal[0].food.length !== 0){
+      this.setState({
+        foods: meal[0].food
+      }, () => {
+        console.log(this.state.foods);
+      })
+    }
+  }
+
   render () {
     return (
-      <div>
-        <h1>Meal Tracker</h1>
-        <div>
-          <div>
-            {this.state.trackers.map( tracker => (
-              <div key={tracker.id}>
-                <h3>Tracker</h3> 
-                <div>{tracker.trackerName}</div>
-
-                <div>
-                  {tracker.meals ? (
-                    <div>
-                      <h3>Meals</h3> 
-                      {tracker.meals.map( meal => (
-                        <div key={meal.id}>
-                          <div>{meal.mealName}</div>
-
-                          <div>
-                            {meal.food ? (
-                              <div>
-                                <h3>Meal Item</h3>
-                                {meal.food.map( food => (
-                                  <div key={food.id}>
-                                    <div>{food.name}</div>                                
-                                  </div>
-                                ))}
-                              </div>
-                            ) : (null)}  
-                          </div>
-
-                        </div>
-                      ))}
-                    </div>
-                  ) : (null)}  
-                </div>
-
-              </div>
-            ))}
-          </div>
-          <div>
-            {this.state.meals.map( meal => (
-              <div key={meal.id}>
-                <div>{meal.mealName}</div>
-              </div>
-            ))}
+      <div className="back">
+        <div className="banner">
+          <div className="banImg">
+            <div className="banTxt">Meal Tracker</div>
           </div>
         </div>
-        
+        <div className="container">
+          <div>
+            <div>
+              {this.state.trackers.map( tracker => (
+                <div key={tracker.id}>
+                  <div className="header">Tracker</div> 
+                  <div>{tracker.trackerName}</div>
+
+                  <div>
+                    {tracker.meals ? (
+                      <div>
+                        <div className="header">Meals</div> 
+                        {tracker.meals.map( meal => (
+                          <div key={meal.id}>
+                            <div>{meal.mealName}</div>
+
+                            <div>
+                              {meal.food ? (
+                                <div>
+                                  <div className="header">Meal Item</div>
+                                  {meal.food.map( food => (
+                                    <div key={food.id}>
+                                      <div>{food.name}</div>                                
+                                    </div>
+                                  ))}
+                                </div>
+                              ) : (null)}  
+                            </div>
+
+                          </div>
+                        ))}
+                      </div>
+                    ) : (null)}  
+                  </div>
+
+                </div>
+              ))}
+            </div>
+            {/* <div>
+              {this.state.meals.map( meal => (
+                <div key={meal.id}>
+                  <div>{meal.mealName}</div>
+                </div>
+              ))}
+            </div> */}
+          </div>
+        </div>
       </div>
     );
   };
