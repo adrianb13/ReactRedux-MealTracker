471c1059a9aa75f947b45ac8435dc01205df3da0
Adrian <coding4adrian@yahoo.com>
2020-02-13 15:55:55 -0800

    update

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,43 +3,43 @@ import axios from "axios";
 export default {
   //Meal Tracker API calls
   getTrackers: () => {
-    return axios.get("http://localhost:8080/api/trackers/");
+    return axios.get("https://mealtracker-java.herokuapp.com/api/trackers/");
   },
   saveTracker: (tracker) => {
-    return axios.post("http://localhost:8080/api/trackers/", tracker);
+    return axios.post("https://mealtracker-java.herokuapp.com/api/trackers/", tracker);
   },
   updateTracker: (tracker) => {
-    return axios.put("http://localhost:8080/api/trackers/" + tracker.id, tracker);
+    return axios.put("https://mealtracker-java.herokuapp.com/api/trackers/" + tracker.id, tracker);
   },
   deleteTracker: (trackerId) => {
-    return axios.delete("http://localhost:8080/api/trackers/" + trackerId);
+    return axios.delete("https://mealtracker-java.herokuapp.com/api/trackers/" + trackerId);
   },
 
   //Meal API calls
   getMeals: (trackerId) => {
-    return axios.get("http://localhost:8080/api/meals/" + trackerId);
+    return axios.get("https://mealtracker-java.herokuapp.com/api/meals/" + trackerId);
   },
   saveMeal: (trackerId, meal) => {
-    return axios.post("http://localhost:8080/api/meals/" + trackerId, meal);
+    return axios.post("https://mealtracker-java.herokuapp.com/api/meals/" + trackerId, meal);
   },
   updateMeal: (trackerId, meal) => {
-    return axios.put("http://localhost:8080/api/meals/" + trackerId + "/" + meal.id, meal);
+    return axios.put("https://mealtracker-java.herokuapp.com/api/meals/" + trackerId + "/" + meal.id, meal);
   },
   deleteMeal: (mealId) => {
-    return axios.delete("http://localhost:8080/api/meals/" + mealId);
+    return axios.delete("https://mealtracker-java.herokuapp.com/api/meals/" + mealId);
   },
 
   //Food API calls
   getFoods: (mealId) => {
-    return axios.get("http://localhost:8080/api/food/" + mealId);
+    return axios.get("https://mealtracker-java.herokuapp.com/api/food/" + mealId);
   },
   saveFood: (mealId, food) => {
-    return axios.post("http://localhost:8080/api/food/" + mealId, food);
+    return axios.post("https://mealtracker-java.herokuapp.com/api/food/" + mealId, food);
   },
   updateFood: (mealId, food) => {
-    return axios.put("http://localhost:8080/api/food/" + mealId+ "/" + food.id, food);
+    return axios.put("https://mealtracker-java.herokuapp.com/api/food/" + mealId+ "/" + food.id, food);
   },
   deleteFood: (foodId) => {
-    return axios.delete("http://localhost:8080/api/food/" + foodId);
+    return axios.delete("https://mealtracker-java.herokuapp.com/api/food/" + foodId);
   }
 }
