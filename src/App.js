e040636189c795b83db93f7ff2e897fcdab73945
Adrian <coding4adrian@yahoo.com>
2020-02-13 16:56:27 -0800

    update

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ class App extends React.Component {
       <Router history={history}>
         <div className="App">
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/add" component={AddForm} />
-            <Route exact path="/add/:trackerId/meal" component = {AddForm} />
-            <Route exact path="/add/:trackerId/:mealId/food" component = {AddForm} />
-            <Route exact path="/update/:trackerId" component = {UpdateDelete} />
-            <Route exact path="/update/:trackerId/:mealId" component = {UpdateDelete} />
-            <Route exact path="/update/:trackerId/:mealId/:foodId" component = {UpdateDelete} />
+            <Route exact path="/ReactRedux-MealTracker/" component={Home} />
+            <Route exact path="/ReactRedux-MealTracker/add" component={AddForm} />
+            <Route exact path="/ReactRedux-MealTracker/add/:trackerId/meal" component = {AddForm} />
+            <Route exact path="/ReactRedux-MealTracker/add/:trackerId/:mealId/food" component = {AddForm} />
+            <Route exact path="/ReactRedux-MealTracker/update/:trackerId" component = {UpdateDelete} />
+            <Route exact path="/ReactRedux-MealTracker/update/:trackerId/:mealId" component = {UpdateDelete} />
+            <Route exact path="/ReactRedux-MealTracker/update/:trackerId/:mealId/:foodId" component = {UpdateDelete} />
             <Route component={Home} />
           </Switch>
         </div>
