e0fa49845ca446aea6f2e73d14ed73a8cfacc40a
Adrian <coding4adrian@yahoo.com>
2020-02-13 21:26:36 -0800

    base url

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
-import history from "./history/history";
+//import history from "./history/history";
+import createBrowserHistory from "history/createBrowserHistory";
 import Home from "./components/Home";
 import AddForm from "./components/Forms/Add";
 import UpdateDelete from "./components/Forms/UpdateDelete";
 
+const history = createBrowserHistory({ basename: "/ReactRedux-MealTracker" });
+
 class App extends React.Component {
   render(){
     return (  
-      <Router history={history} basename="/ReactRedux-MealTracker">
+      <Router history={history}>
         <div className="App">
           <Switch>
             <Route exact path="/" component={Home} />
