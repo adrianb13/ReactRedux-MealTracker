3523a483f557df72734e00dd24cc8ccb38468a1e
Adrian <coding4adrian@yahoo.com>
2020-02-13 16:01:16 -0800

    Updates

---
+++ precache-manifest.f6617ba9fd8aaf0f10afd2766302195f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8577f085dd583dcde7b5173769a925e3",
+    "url": "/ReactRedux-MealTracker/index.html"
+  },
+  {
+    "revision": "a2c9a309b7108564eb85",
+    "url": "/ReactRedux-MealTracker/static/css/main.213cda57.chunk.css"
+  },
+  {
+    "revision": "46f5ee8b64f8d7ed13f4",
+    "url": "/ReactRedux-MealTracker/static/js/2.fa2500c7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ReactRedux-MealTracker/static/js/2.fa2500c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c9a309b7108564eb85",
+    "url": "/ReactRedux-MealTracker/static/js/main.64ac07bf.chunk.js"
+  },
+  {
+    "revision": "842912c7246f0ef55248",
+    "url": "/ReactRedux-MealTracker/static/js/runtime-main.0a0600bd.js"
+  },
+  {
+    "revision": "a1aff6c375f4e54f7cac3cab25b11196",
+    "url": "/ReactRedux-MealTracker/static/media/banner.a1aff6c3.png"
+  },
+  {
+    "revision": "dcf0a3a9882166381f5dc8301c641b25",
+    "url": "/ReactRedux-MealTracker/static/media/wood.dcf0a3a9.jpg"
+  }
+]);
